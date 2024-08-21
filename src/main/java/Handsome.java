14585e6aa8b2821817e553025c3e40ca49e58a8f
siriousguy <raylisr@gmail.com>
2024-08-21 17:04:38 +0800

    Changed main class name and added greeting messages.

---
+++ src/main/java/Handsome.java
@@ -0,0 +1,13 @@
+public class Handsome {
+    public static void main(String[] args) {
+        /* String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n"; */
+
+
+        System.out.println("Hello! I'm Handsome\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
