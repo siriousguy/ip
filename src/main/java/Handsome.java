f6849fbc7e6d801378189bb301f8910027769467
siriousguy <raylisr@gmail.com>
2024-08-22 18:44:25 +0800

    Implemented the functions of add and list.

--- src/main/java/Handsome.java
+++ src/main/java/Handsome.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Handsome {
+    private static final String[] data = new String[100];
+    private static int dataCount = 0;
+
     public static void main(String[] args) {
         /* String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,12 +18,20 @@ public class Handsome {
         while (true) {
             String input = scanner.nextLine();
 
+            if (input.equals("list")) {
+                for (int i = 1; i <= dataCount; i++) {
+                    System.out.println(i + ". " + data[i - 1]);
+                }
+            } else {
+                data[dataCount] = input;
+                dataCount++;
+                System.out.println("added: " + input);
+            }
+
             if (input.equals("bye")) {
                 System.out.println("Bye, it was an amazing conversation!");
                 break;
             }
-            System.out.println(input);
-
         }
     }
 }
